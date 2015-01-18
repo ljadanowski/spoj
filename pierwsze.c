9ab45943f0f001d787834dc485ef3a31ee59d862
Lukasz Jadanowski <lukasz.jadanowski@gmail.com>
2015-01-18 08:53:53 +0100

    poprawna wersja wg. spoja

--- pierwsze.c
+++ pierwsze.c
@@ -3,27 +3,28 @@
 void czy_pierwsza(int liczba);
 int main() 
 {
-  int n, i = 0, pom;
-  while(scanf("%d", &n) != 1 || n > 100000 )
-  continue;
- 
-  int *wsk = (int*) malloc(sizeof(int)*n);
+  int n, pom;
+
+  scanf("%d", &n);
+  //int *wsk = (int*) malloc(sizeof(int)*n);
   
-  while(i<n)
+  while(n--)
   {
 	  scanf("%d", &pom);
-	  if(pom >= 1 && pom <= 10000) wsk[i++] = pom;
+	  czy_pierwsza(pom);
+	  //wsk[i++] = pom;
   }
 
-  for(i=0; i<n; i++) czy_pierwsza(wsk[i]);
+  //for(i=0; i<n; i++) czy_pierwsza(wsk[i]);
   
   return 0;
+
 }
 void czy_pierwsza(int liczba)
 {
   int i;
-  if(liczba<2) puts("NIE");
-  else if(liczba == 1) puts("TAK");
+  if(liczba<2) { puts("NIE"); }
+  else if(liczba == 2) puts("TAK");
   else 
   {
     for(i=2; i<liczba; i++) 
@@ -31,6 +32,6 @@ void czy_pierwsza(int liczba)
       if(liczba%i == 0) { puts("NIE"); break; }
       else if(liczba == i+1) { puts("TAK"); break; }
     }
-  } 
+  }
 }
 
